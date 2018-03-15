7374655e7572e6aed3c4fe8b8490f039cc998e46
Chris Zieba <zieba.chris@gmail.com>
2018-03-15 09:44:31 -0700

    Update song list

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
-
 const parser = new xml2js.Parser();
-
-
 const file = 'rhythmdb.xml';
 
 fs.readFile(`${__dirname}/${file}`, (err, data) => {
   parser.parseString(data, (err, result) => {
     const builder = new xml2js.Builder();
-    let title = '';
-    let artist = '';
 
-    
     const songs = result.rhythmdb.entry.reduce((songs, song) => {
-      console.log(song)
+      let songLocation = song.location[0].split("/");
+      songLocation = songLocation[songLocation.length-1].replace(/\.[^/.]+$/, "");
+      console.log(songLocation)
+      songLocation = songLocation.split('-');
+      let title = songLocation[1].replace(/%20/g, " ").trim();
+      let artist = songLocation[0].replace(/%20/g, " ").trim();
+
       songs.push({
+        $: { type: "song"},
         title: title,
         genre: "Unknown",
         artist: artist,
@@ -23,8 +24,8 @@ fs.readFile(`${__dirname}/${file}`, (err, data) => {
         duration: song.duration[0],
         "file-size": song["file-size"][0],
         "location": song.location[0],
-        bitrate: song.bitrate[0],
-        "first-seen":, song["first-seen"][0],
+        mtime: song.mtime[0],
+        "first-seen": song["first-seen"][0],
         "last-seen": song["last-seen"][0], 
         bitrate: song.bitrate[0],
         date: song.date[0],
@@ -36,7 +37,7 @@ fs.readFile(`${__dirname}/${file}`, (err, data) => {
     const obj = {
       rhythmdb: {
         $: {version: "1.8"},
-        song: [{g:4},{g:6}],
+        entry: songs,
       }
     };
 
